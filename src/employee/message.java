29eb2c2b61821a8f0025b6b25beac248118021e4
sumithragithubrit <115725079+sumithragithubrit@users.noreply.github.com>
2022-11-23 11:58:19 +0530

    Update message.java

--- src/employee/message.java
+++ src/employee/message.java
@@ -5,8 +5,7 @@ public class message {
 	static final int WAGE_PER_HOUR = 20;
     static final int FULL_DAY_HOUR = 8;
     static final int PART_TIME_HOUR = 4;
-    static final int IS_FULL_TIME = 2;
-    static final int IS_PART_TIME = 1;
+    
 	public static void main(String[] args) {
 		System.out.println("Welcome to employee wage");
         int employeeCheck = (int) Math.floor(Math.random()*10)%3;
