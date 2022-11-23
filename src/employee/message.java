7e032937462f71528f0b01f5ea3970b8fe5d5fad
sumi <sumipavib1302@gmail.com>
2022-11-23 10:43:44 +0530

    DailyWage

--- src/employee/message.java
+++ src/employee/message.java
@@ -2,22 +2,26 @@ package employee;
 import java.util.Random;
 
 public class message {
+	static final int WAGE_PER_HOUR = 20;
+
+	static final int FULL_DAY_HOUR = 8;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
 		int employeecheck=(int)Math.floor(Math.random()*10)%2;
-		
+	int dailywage=0;
         System.out.println(employeecheck);
-        if(employeecheck==0)
+        if(employeecheck==1)
         {
         	System.out.println("Employee is present");
-        	
+        	 int dailyWage = FULL_DAY_HOUR * WAGE_PER_HOUR; 
+        	 
         }
        
         else {
         	System.out.println("Employee is absent");
         }
-        
+        System.out.println("Employee Daily Wage is"+dailywage);
 	}
 
 }
