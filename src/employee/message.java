626cb288dcd63dc8d87f93d76104ea4133dbd245
sumi <sumipavib1302@gmail.com>
2022-11-23 11:46:47 +0530

    parttime wage

--- src/employee/message.java
+++ src/employee/message.java
@@ -1,27 +1,29 @@
 package employee;
-import java.util.Random;
+
 
 public class message {
 	static final int WAGE_PER_HOUR = 20;
-
-	static final int FULL_DAY_HOUR = 8;
-
+    static final int FULL_DAY_HOUR = 8;
+    static final int PART_TIME_HOUR = 4;
+    static final int IS_FULL_TIME = 2;
+    static final int IS_PART_TIME = 1;
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-		int employeecheck=(int)Math.floor(Math.random()*10)%2;
-	int dailywage=0;
-        System.out.println(employeecheck);
-        if(employeecheck==1)
-        {
-        	System.out.println("Employee is present");
-        	 int dailyWage = FULL_DAY_HOUR * WAGE_PER_HOUR; 
-        	 
+		System.out.println("Welcome to employee wage");
+        int employeeCheck = (int) Math.floor(Math.random()*10)%3;
+        System.out.println(employeeCheck);
+        int dailyWage = 0;
+        if(employeeCheck == 1) {
+            dailyWage = PART_TIME_HOUR * WAGE_PER_HOUR;
+            System.out.println("Employee is part time ");
+        } else if (employeeCheck == 2) {
+            dailyWage =FULL_DAY_HOUR * WAGE_PER_HOUR;
+            System.out.println("Employee is full time ");
+        }else {
+
+                System.out.println("Employee is Absent");
         }
-       
-        else {
-        	System.out.println("Employee is absent");
-        }
-        System.out.println("Employee Daily Wage is"+dailywage);
-	}
+        System.out.println("Employee daily wage is " + dailyWage);
+    }
+
 
 }
